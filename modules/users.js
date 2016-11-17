97f1f222a5ef45eba453c551a2aed247850793cf
pierric cotton <pierric cotton>
2016-11-17 11:30:48 +0100

    correction user

--- modules/users.js
+++ modules/users.js
@@ -1,16 +1,8 @@
 var userJson = require('../json/users.json');
 
-var connexion = function (login, callback){
-    for(var key in userJson){
-      if(userJson[key].user_id === login){
-        callback(null, userJson[key]);
-      }
-    }
+var get_user_by_login = function (login, callback){
+    callback(null,userJson[login]);
 }
 
-var get_user = function (login,callback){
 
-}
-
-exports.connexion = connexion;
-exports.get_user = get_user;
+exports.get_user_by_login = get_user_by_login;
