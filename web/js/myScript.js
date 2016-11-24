4784cf7e9d9c9cc0afe3f5314a00a01e6ca281f0
pierric cotton <pierric cotton>
2016-11-24 11:37:48 +0100

    front end

--- web/js/myScript.js
+++ web/js/myScript.js
@@ -6,6 +6,19 @@ $(document).ready(function() {
 
     //io.connect('http://localhost:3000');
     io.connect('https://gengisskhan.herokuapp.com/');
-    $("#tableCourses").DataTable();
+    $("#tableCourses").DataTable({
+    
+      "aoColumns": [
+              null,
+              null,
+              {"bSortable": false},
+              {"bSortable": false},
+              {"bSortable": false},
+              null,
+              null,
+              null,
+              {"bSortable": false}
+          ]
+    });
     return;
 });
